caa4f4224e96b8b434904b0dc8c90023ad89092b
정호심 <ghtla0330@naver.com>
2022-07-02 16:56:31 +0900

    메뉴 전환 테스트_비건, 케이크, 쿠키

---
+++ app/src/main/java/com/example/umc77/CookiesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.umc77
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CookiesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cookies)
+    }
+}
--- app/src/main/java/com/example/umc77/MainActivity.kt
+++ app/src/main/java/com/example/umc77/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.umc77
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.umc77.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.menuVeganIv.setOnClickListener {
+            val intent = Intent(this,VeganActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.menuCakeIv.setOnClickListener {
+            val intent = Intent(this,CakeActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.menuCookiesIv.setOnClickListener {
+            val intent = Intent(this,CookiesActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
