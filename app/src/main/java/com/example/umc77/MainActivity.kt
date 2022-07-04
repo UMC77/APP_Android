cf8a28d2a570a1e3fdac20e344fe09eb9572b3be
Leesoo <altnfsudwo@naver.com>
2022-07-04 16:57:49 +0900

    gradle,MainActivity 주석추가

--- app/src/main/java/com/example/umc77/MainActivity.kt
+++ app/src/main/java/com/example/umc77/MainActivity.kt
@@ -6,42 +6,38 @@ import android.os.Bundle
 import com.example.umc77.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        //바인딩 겍체 획득
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        //액티비티 화면 출력
         setContentView(binding.root)
 
+        //뷰 객체 이용
+        //안드로이드 스튜디오에서는 id에서 설정한 _를 자동으로 생략
         binding.menuVeganIv.setOnClickListener {
             val intent = Intent(this,VeganActivity::class.java)
             startActivity(intent)
-
         }
 
         binding.menuCakeIv.setOnClickListener {
             val intent = Intent(this,CakeActivity::class.java)
             startActivity(intent)
-
         }
 
         binding.menuCookiesIv.setOnClickListener {
             val intent = Intent(this,CookiesActivity::class.java)
             startActivity(intent)
-
         }
 
         binding.menuDonutIv.setOnClickListener {
             val intent = Intent(this,DonutActivity::class.java)
             startActivity(intent)
-
         }
 
         binding.menuBreadIv.setOnClickListener {
             val intent = Intent(this,BreadActivity::class.java)
             startActivity(intent)
-
         }
     }
 }
